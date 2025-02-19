d36b47e2001f534075468afa52342c537a389c81
Ricardo Carrapito <ricardocarrapito.dev@gmail.com>
2025-02-19 20:45:56 +0000

    add char

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -10,10 +10,18 @@ namespace VariousTypes
             int valor1= 1;
             int valor2= 2;
             int valor3= 3;
-            
+
             Console.WriteLine(valor1);
             Console.WriteLine(valor2);
             Console.WriteLine(valor3);
+
+            char checksymbol= '\u2705';
+            char arrow= '\u27A1';
+            char cross= '\u2612';
+
+            Console.WriteLine(checksymbol);
+            Console.WriteLine(arrow);
+            Console.WriteLine(cross);
         }
     }
 }
