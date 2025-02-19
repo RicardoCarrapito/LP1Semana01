fd63e73920e85b40b890e47d3c33d4dd859ad4ea
Ricardo Carrapito <ricardocarrapito.dev@gmail.com>
2025-02-19 20:33:58 +0000

    add int

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        
+        private static void Main(string[] args)
+        {
+            int valor1= 1;
+            int valor2= 2;
+            int valor3= 3;
+            
+            Console.WriteLine(valor1);
+            Console.WriteLine(valor2);
+            Console.WriteLine(valor3);
+        }
+    }
+}
