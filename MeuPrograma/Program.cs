3c461716ca0e0f76144136a3c5a722aac7179232
Ricardo Carrapito <ricardocarrapito.dev@gmail.com>
2025-02-19 20:17:23 +0000

    Add commentary

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -4,6 +4,10 @@ namespace MeuPrograma
 {
     public class Program
     {
+        /// <summary>
+        /// start class main
+        /// </summary>
+        /// <param name="args">space for a commentary </param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
