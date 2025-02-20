3460b10c1508abda871ffff224d8880a941b2b49
Ricardo Carrapito <ricardocarrapito.dev@gmail.com>
2025-02-20 16:30:42 +0000

    ManyStrings Ex4

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string smile = "this is a star \u2606";
+            string cross="this is a cross \u274c";
+            string heart= "This is a Heart:\u2661";
+
+            Console.WriteLine(smile);
+            Console.WriteLine(cross);
+            Console.WriteLine(heart);
+        }
+    }
+}
