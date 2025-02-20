548f49f6365b38fed4533564125afeb6c0fb624b
Ricardo Carrapito <ricardocarrapito.dev@gmail.com>
2025-02-20 17:18:22 +0000

    AskForInput second convert

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -9,8 +9,12 @@ namespace AskForInput
             Console.WriteLine("Introduza um numero inteiro:");
             string str1= Console.ReadLine();
             int inteiro= int.Parse(str1);
-            
 
+            Console.WriteLine("Introduza um numero real:");
+            string str2 = Console.ReadLine();
+            float real = float.Parse(str2);
+            
+             
 
         }
     }
