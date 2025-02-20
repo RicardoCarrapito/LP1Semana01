3cec5482ac93c4f351da4292662bad61184770b0
Ricardo Carrapito <ricardocarrapito.dev@gmail.com>
2025-02-20 17:14:44 +0000

    AskForInput First convert

---
+++ AskForInput/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AskForInput
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Introduza um numero inteiro:");
+            string str1= Console.ReadLine();
+            int inteiro= int.Parse(str1);
+            
+
+
+        }
+    }
+}
