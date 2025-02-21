2f2f91109496db49709b778e708c80aaee15beeb
Ricardo Carrapito <ricardocarrapito.dev@gmail.com>
2025-02-21 14:11:39 +0000

    recepcao numeros e conversao

---
+++ Percentagem/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Percentagem
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Insere 4 numeros reais entre 0 e 1:");
+            string num1= Console.ReadLine();
+            string num2= Console.ReadLine();
+            string num3= Console.ReadLine();
+            string num4= Console.ReadLine();
+
+            float perc1= float.Parse(num1);
+            float perc2= float.Parse(num2);
+            float perc3= float.Parse(num3);
+            float perc4= float.Parse(num4);
+            
+
+
+        
+        }
+    }
+}
